08149250bc8f8369d2f316abb6d16bce696a995d
Richard Burd <richardburdx@yahoo.com>
2018-02-22 04:39:34 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,16 @@
-class UsersController < ApplicationController 
-  get 'signup' do 
-    erb :"users/new" 
-  end 
+class UsersController < ApplicationController # This is just me copying from 1:09:00 / 1:28:48 from this video here: https://www.youtube.com/watch?time_continue=2&v=_S1s6R-_wYc
+  get 'signup' do
+    erb :"users/new"
+  end
 
-  post '/users' do 
-    @user = User.new 
-    @user.email = params[:email] 
-    @user.password = params[:password] 
-    if @user.save 
-      redirect '/login' 
-    else 
+  post '/users' do
+    @user = User.new
+    @user.email = params[:email]
+    @user.password = params[:password]
+    if @user.save
+      redirect '/login'
+    else
       erb :"users/new"
-    end 
-  end 
-end 
+    end
+  end
+end
