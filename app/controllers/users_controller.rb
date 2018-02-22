e2f0b07b29f450b43aa0e67ee5c2df5fe3ef1b52
Richard Burd <richardburdx@yahoo.com>
2018-02-22 04:50:55 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,3 @@
-require './config/environment'
 require_relative 'application_controller'
 class UsersController < ApplicationController # This is just me copying from 1:09:00 / 1:28:48 from this video here: https://www.youtube.com/watch?time_continue=2&v=_S1s6R-_wYc
   get '/signup' do
