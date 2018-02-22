e144cbe3277ea2b623278fc306fad7fe42460291
Richard Burd <richardburdx@yahoo.com>
2018-02-22 04:57:38 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171102201815) do
+ActiveRecord::Schema.define(version: 20180222045214) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string  "title"
+    t.text    "content"
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
